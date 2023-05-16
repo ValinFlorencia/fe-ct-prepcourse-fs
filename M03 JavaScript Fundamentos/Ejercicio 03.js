3d5c27aa9f5cae9f42441538c0e1e0987030f7d1
Florencia <valinflorencia@gmail.com>
2023-05-16 15:51:15 -0300

    Módulo tres completado

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,73 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   if (x === y){
+      return true ;
+   } else {
+      return false ;
+   }
 }
+
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
-}
+   if (str1. length === str2. length){
+      return true ;
+   } else { 
+      return false ;
+   }
+   }
+
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   var noventa = (90)
+   if (num < noventa){
+      return true ;
+   } else {
+      return false ;
+   }
+   
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   var cincuenta = (50)
+   if (num > cincuenta) {
+      return true ;
+   } else {
+      return false ;
+   }
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
-}
+   var par = (num % 2 == 0) ;
+   if (num = par){
+      return true ;
+   } else {
+      return false ;
+   }
+}    
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   var impar = (num % 2 !== 0) ;
+   if (num = impar){
+      return true ;
+   } else {
+      return false ;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
